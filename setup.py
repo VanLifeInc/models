6af1d7219a3b04886cc0e9f407f232cc62ca65f8
Currie32 <david.currie32@gmail.com>
2018-07-29 20:41:33 +0100

    Reformat imports

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import os
 from codecs import open
 
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import find_packages, setup
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 PATH_VERSION = os.path.join(HERE, '__version__.py')
-
 
 about = {}
 with open(PATH_VERSION, 'r', 'utf-8') as f:
